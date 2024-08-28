3cbc3873929b39e2541aca59b9b69ef2472d18ab
Daniel Almeida <danielf.almeida@gmail.com>
2024-08-28 18:39:28 +0100

    Initial Commit

---
+++ pix-flix/pix_flixApp.swift
@@ -0,0 +1,32 @@
+//
+//  pix_flixApp.swift
+//  pix-flix
+//
+//  Created by Daniel Almeida on 28/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct pix_flixApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
